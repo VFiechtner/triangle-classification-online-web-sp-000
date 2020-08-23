e8a950e830f5983769fc2f83f1599b5d75288b47
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-08-23 22:21:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,11 @@ class Triangle
         end
     elsif s1 == s2 && s1 == s3
       self.kind = :equilateral
-    elsif s1 == s2 || 
+    elsif s1 == s2 || s1 == s3 || s2 == s3
+      self.kind = :isosceles
+    else 
+      self.kind = :scalene
+    end
   end
 
   class TriangleError < StandardError
