138fbf0f25e068631764e22647774c4b50094cc4
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-08-23 22:13:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,13 @@ class Triangle
     @s3 = s3
   end
 
+  def kind
+    if (@s1 + @s2) <= @s3 || (@s2 + @s3) <= @s1 || (@s3 + @s1) <= @s2
+
+      raise TriangleError
+      
+  end
+
   class TriangleError < StandardError
 
   end
